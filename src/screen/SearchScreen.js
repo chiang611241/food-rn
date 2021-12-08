b169a8745d108d0f6ab86b2c11ee3bc9b3f0fd61
JennyChiang <chihyueh871125@gmail.com>
2021-12-08 09:50:46 +0800

    handle: add error hanlde

--- src/screen/SearchScreen.js
+++ src/screen/SearchScreen.js
@@ -6,16 +6,21 @@ import SearchBar from '../components/SearchBar';
 const SearchScreen = () => {
   const [term, setTerm] = useState('');
   const [results, setResults] = useState([]);
+  const [errorMessage, setErrorMessage] = useState('');
 
   const searchApi = async () => {
-    const response = await yelp.get('/search', {
-      params: {
-        limit: 50,
-        term,
-        location: 'san jose'
-      }
-    });
-    setResults(response.data.businesses);
+    try {
+      const response = await yelp.get('/search', {
+        params: {
+          limit: 50,
+          term,
+          location: 'san jose',
+        },
+      });
+      setResults(response.data.businesses);
+    } catch (error) {
+      setErrorMessage(error);
+    }
   };
 
   return (
@@ -25,8 +30,8 @@ const SearchScreen = () => {
         onTermChange={(newTerm) => setTerm(newTerm)}
         onTermSubmit={() => searchApi()}
       />
-      <Text>Search</Text>
-      <Text>{results.length}</Text>
+      {errorMessage ? <Text>{errorMessage}</Text> : null}
+      <Text>We have found {results.length} results.</Text>
     </View>
   );
 };
