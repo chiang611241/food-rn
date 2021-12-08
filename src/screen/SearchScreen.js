50f5a34d1a7877b0c19063e34f16e4ded01ff7d4
JennyChiang <chihyueh871125@gmail.com>
2021-12-08 09:40:06 +0800

    feat: add search api function

--- src/screen/SearchScreen.js
+++ src/screen/SearchScreen.js
@@ -1,18 +1,32 @@
 import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import yelp from '../api/yelp';
 import SearchBar from '../components/SearchBar';
 
 const SearchScreen = () => {
   const [term, setTerm] = useState('');
+  const [results, setResults] = useState([]);
+
+  const searchApi = async () => {
+    const response = await yelp.get('/search', {
+      params: {
+        limit: 50,
+        term,
+        location: 'san jose'
+      }
+    });
+    setResults(response.data.businesses);
+  };
+
   return (
     <View>
       <SearchBar
         term={term}
         onTermChange={(newTerm) => setTerm(newTerm)}
-        onTermSubmit={() => {}}
+        onTermSubmit={() => searchApi()}
       />
       <Text>Search</Text>
-      <Text>{term}</Text>
+      <Text>{results.length}</Text>
     </View>
   );
 };
