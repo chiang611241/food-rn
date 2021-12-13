de3b6a055ab9a7d453c8f5ef117462856ae1d26d
JennyChiang <chihyueh871125@gmail.com>
2021-12-13 13:52:54 +0800

    handle: add result detail component

--- src/components/ResultList.js
+++ src/components/ResultList.js
@@ -1,27 +1,32 @@
 import React from 'react';
 import { View, Text, StyleSheet, FlatList } from 'react-native';
+import ResultDetail from './ResultDetail';
 
 const ResultList = ({ title, results }) => {
   return (
-    <View>
+    <View style={styles.container}>
       <Text style={styles.title}>{title}</Text>
       <FlatList
         horizontal
+        showsHorizontalScrollIndicator={false}
         data={results}
         keyExtractor={(result) => result.id}
-        renderItem={({ item }) => (
-            <Text>{item.name}</Text>
-        )}
+        renderItem={({ item }) => <ResultDetail result={item} />}
       />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-    title: {
-        fontSize: 18,
-        fontWeight: 'bold',
-    },
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginLeft: 15,
+    marginBottom: 5,
+  },
+  container: {
+    marginBottom: 10,
+  },
 });
 
 export default ResultList;
