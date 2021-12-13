b13ed97bdba06ba3940b2bf13a2dc59378f93e12
JennyChiang <chihyueh871125@gmail.com>
2021-12-13 14:02:31 +0800

    fix: search screen layouyt

--- src/screen/SearchScreen.js
+++ src/screen/SearchScreen.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, ScrollView } from 'react-native';
 import useResults from '../hooks/useResults';
 import SearchBar from '../components/SearchBar';
 import ResultList from '../components/ResultList';
@@ -13,18 +13,19 @@ const SearchScreen = () => {
   };
 
   return (
-    <View>
+    <>
       <SearchBar
         term={term}
         onTermChange={(newTerm) => setTerm(newTerm)}
         onTermSubmit={() => searchApi(term)}
       />
       {errorMessage ? <Text>{errorMessage}</Text> : null}
-      <Text>We have found {results.length} results.</Text>
-      <ResultList results={filterResultByPrice('$')} title='Cost Effective' />
-      <ResultList results={filterResultByPrice('$$')} title='Bit Pricier' />
-      <ResultList results={filterResultByPrice('$$$')} title='Big Spender' />
-    </View>
+      <ScrollView>
+        <ResultList results={filterResultByPrice('$')} title='Cost Effective' />
+        <ResultList results={filterResultByPrice('$$')} title='Bit Pricier' />
+        <ResultList results={filterResultByPrice('$$$')} title='Big Spender' />
+      </ScrollView>
+    </>
   );
 };
 
